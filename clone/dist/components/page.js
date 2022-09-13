4e019a2db0eb16590a8f6515350a612b81b20e5a
mason park <joung4342@gmail.com>
2022-09-13 20:51:05 +0900

    add clone pageComponent

---
+++ clone/dist/components/page.js
@@ -0,0 +1,11 @@
+class PageComponent {
+    constructor() {
+        this.element = document.createElement('ul');
+        this.element.setAttribute('class', 'page');
+        this.element.textContent = 'This is PageComponent';
+    }
+    attaachTo(parent, position = 'afterbegin') {
+        parent.insertAdjacentElement(position, this.element);
+    }
+}
+export { PageComponent };
