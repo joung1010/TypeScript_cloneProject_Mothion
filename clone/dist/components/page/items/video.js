74639a234abe77faf9695708140c134379d18183
mason park <joung4342@gmail.com>
2022-09-15 21:07:45 +0900

    clone video item 보완(정규표현식)

--- clone/dist/components/page/items/video.js
+++ clone/dist/components/page/items/video.js
@@ -7,13 +7,19 @@ export class VideoComponent extends BaseComponent {
           </div>
             <h3 class="video__title"></h3>
         </section>`);
-        this.embedURL = 'https://www.youtube.com/embed/';
         const video = this.element.querySelector('.video__iframe');
-        const idx = url.lastIndexOf('/') + 1;
-        const videoId = url.slice(idx);
-        video.src = this.embedURL + videoId;
+        video.src = this.convertToEmbeddedURL(url);
         video.title = titile;
         const title = this.element.querySelector('.video__title');
         title.textContent = titile;
+    }
+    convertToEmbeddedURL(url) {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+        const videoId = match ? match[1] || match[2] : undefined;
+        if (videoId) {
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+        return url;
     }
 }
--- clone/src/components/page/items/video.ts
+++ clone/src/components/page/items/video.ts
@@ -1,6 +1,5 @@
 import {BaseComponent} from '../../component.js'
 export class VideoComponent extends BaseComponent<HTMLElement>{
-    private embedURL :string = 'https://www.youtube.com/embed/';
     constructor(titile:string, url:string) {
         super(`<section class="video">
          <div class="video__player">
@@ -9,12 +8,24 @@ export class VideoComponent extends BaseComponent<HTMLElement>{
             <h3 class="video__title"></h3>
         </section>`);
         const video = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        const idx = url.lastIndexOf('/') +1;
-        const videoId = url.slice(idx)
-        video.src = this.embedURL+videoId; // url -> videoId -> embed
+        video.src = this.convertToEmbeddedURL(url); // url -> videoId -> embed
         video.title=titile;
         const title = this.element.querySelector('.video__title')! as HTMLParagraphElement;
         title.textContent = titile;
 
     }
+
+    private convertToEmbeddedURL(url: string):string {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+        const videoId = match ? match[1] || match[2] : undefined;
+        if (videoId) {
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+        return url;
+    }
+
 }
+
+//https://www.youtube.com/watch?v=qtlWnuv3TF4&ab_channel=%ED%83%AC%ED%83%AC%EB%B2%84%EB%A6%B0
+//https://youtu.be/qtlWnuv3TF4
