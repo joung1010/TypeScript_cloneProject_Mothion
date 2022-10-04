49873841a96476359f79cf0831bfc484bbcde848
mason park <joung4342@gmail.com>
2022-10-04 21:34:27 +0900

    add myself drag and drop 1

--- myself/build/components/page/page.js
+++ myself/build/components/page/page.js
@@ -1,7 +1,7 @@
 import { BaseComponent } from '../component.js';
 export class PageItemComponent extends BaseComponent {
     constructor() {
-        super(`<li class="page_iem">
+        super(`<li class="page_iem" draggable="true">
                <section class="page_item__body">
                    <div class="page_item__controls">
                        <button class="close">&times;</button>
@@ -12,6 +12,38 @@ export class PageItemComponent extends BaseComponent {
         close.onclick = () => {
             this.closeListene && this.closeListene();
         };
+        this.element.addEventListener('dragstart', (event) => {
+            this.onDragStart(event);
+        });
+        this.element.addEventListener('dragend', (event) => {
+            this.onDragEnd(event);
+        });
+        this.element.addEventListener('dragover', (event) => {
+            this.onDragOver(event);
+        });
+        this.element.addEventListener('dragleave', (event) => {
+            this.onDragLeave(event);
+        });
+    }
+    onDragStart(event) {
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData('text/html', this.element.innerHTML);
+        this.onDragObserver(event, 'start');
+    }
+    onDragEnd(event) {
+        this.onDragObserver(event, 'end');
+    }
+    onDragOver(event) {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+        this.onDragObserver(event, 'over');
+    }
+    onDragLeave(event) {
+        event.stopPropagation();
+        this.onDragObserver(event, 'leave');
+    }
+    onDragObserver(event, state) {
+        this.dragListener && this.dragListener(this, state, event);
     }
     addChild(child) {
         const container = this.element.querySelector('.page_item__body');
@@ -20,11 +52,31 @@ export class PageItemComponent extends BaseComponent {
     setOnCloseListener(listener) {
         this.closeListene = listener;
     }
+    setOnDragListener(listener) {
+        this.dragListener = listener;
+    }
 }
 export class PageComponent extends BaseComponent {
     constructor(pageItemConstructor) {
         super(`<ul class="page"></ul>`);
         this.pageItemConstructor = pageItemConstructor;
+        this.element.addEventListener('dragover', (event) => {
+            this.onDragOver(event);
+        });
+        this.element.addEventListener('drop', (event) => {
+            this.onDrop(event);
+        });
+    }
+    onDragOver(event) {
+        event.preventDefault();
+    }
+    onDrop(event) {
+        event.preventDefault();
+        if (!this.dropElement || !this.dragElement) {
+            return;
+        }
+        if (this.dragElement !== this.dropElement) {
+        }
     }
     addChild(component) {
         const item = new this.pageItemConstructor();
@@ -32,6 +84,24 @@ export class PageComponent extends BaseComponent {
         item.attaachTo(this.element, 'beforeend');
         item.setOnCloseListener(() => {
             item.removeFrom(this.element);
+        });
+        item.setOnDragListener((target, state, event) => {
+            switch (state) {
+                case "start":
+                    this.dragElement = target;
+                    break;
+                case "end":
+                    this.dragElement = undefined;
+                    break;
+                case "over":
+                    this.dropElement = target;
+                    break;
+                case "leave":
+                    this.dropElement = undefined;
+                    break;
+                default:
+                    throw new Error(`There is no Options ${state}`);
+            }
         });
     }
 }
--- myself/src/components/page/page.ts
+++ myself/src/components/page/page.ts
@@ -5,6 +5,8 @@ export interface Composable {
 }
 
 type OnCloseListener = () => void;
+type OnDragListener<T extends Component> = (target:T,state:DragState,event:DragEvent) => void;
+type DragState = 'start'|'end'|'over'|'leave';
 
 type SectionConstructor<T extends SectionContainer> = {
     new():T
@@ -12,12 +14,14 @@ type SectionConstructor<T extends SectionContainer> = {
 
 interface SectionContainer extends Component,Composable {
     setOnCloseListener(listener : OnCloseListener) :void
+    setOnDragListener(listener: OnDragListener<SectionContainer>):void;
 }
 
 export class PageItemComponent extends BaseComponent<HTMLLIElement> implements SectionContainer{
     private closeListene? : OnCloseListener;
+    private dragListener? : OnDragListener<SectionContainer>;
     constructor() {
-        super(`<li class="page_iem">
+        super(`<li class="page_iem" draggable="true">
                <section class="page_item__body">
                    <div class="page_item__controls">
                        <button class="close">&times;</button>
@@ -29,6 +33,45 @@ export class PageItemComponent extends BaseComponent<HTMLLIElement> implements S
         close.onclick = () => {
             this.closeListene && this.closeListene();
         }
+
+        this.element.addEventListener('dragstart',(event:DragEvent)=>{
+            this.onDragStart(event);
+        });
+
+        this.element.addEventListener('dragend',(event:DragEvent)=>{
+            this.onDragEnd(event);
+        });
+
+        this.element.addEventListener('dragover',(event:DragEvent)=>{
+            this.onDragOver(event);
+        });
+
+        this.element.addEventListener('dragleave',(event:DragEvent)=>{
+            this.onDragLeave(event);
+        });
+
+    }
+
+    onDragStart(event:DragEvent) {
+        event.dataTransfer!.effectAllowed = 'move';
+        event.dataTransfer!.setData('text/html', this.element.innerHTML);
+        this.onDragObserver(event,  'start');
+    }
+    onDragEnd(event:DragEvent) {
+        this.onDragObserver(event,  'end');
+    }
+    onDragOver(event:DragEvent) {
+        event.preventDefault();
+        event.dataTransfer!.dropEffect = 'move';
+        this.onDragObserver(event,  'over');
+    }
+    onDragLeave(event:DragEvent) {
+        event.stopPropagation();
+        this.onDragObserver(event,  'leave');
+    }
+
+    private onDragObserver(event:DragEvent,state:DragState) {
+        this.dragListener && this.dragListener(this,state,event);
     }
 
     addChild(child: Component) {
@@ -39,11 +82,38 @@ export class PageItemComponent extends BaseComponent<HTMLLIElement> implements S
     setOnCloseListener(listener : OnCloseListener) {
         this.closeListene = listener;
     }
+
+    setOnDragListener(listener: OnDragListener<SectionContainer>) {
+        this.dragListener = listener;
+    }
+
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement>{
+    private dragElement?: SectionContainer;
+    private dropElement?: SectionContainer;
     constructor(private pageItemConstructor:SectionConstructor<SectionContainer> ) {
         super(`<ul class="page"></ul>`);
+        this.element.addEventListener('dragover',(event:DragEvent)=>{
+            this.onDragOver(event);
+        });
+        this.element.addEventListener('drop',(event:DragEvent)=>{
+            this.onDrop(event);
+        });
+    }
+
+    onDragOver(event:DragEvent) {
+        event.preventDefault();
+    }
+
+    onDrop(event:DragEvent) {
+        event.preventDefault();
+        if (!this.dropElement || !this.dragElement) {
+            return;
+        }
+        if (this.dragElement !== this.dropElement) {
+
+        }
     }
 
     addChild(component:Component) {
@@ -52,6 +122,24 @@ export class PageComponent extends BaseComponent<HTMLUListElement>{
         item.attaachTo(this.element, 'beforeend');
         item.setOnCloseListener(() => {
             item.removeFrom(this.element);
+        });
+        item.setOnDragListener((target,state,event)=>{
+            switch (state) {
+                case "start":
+                    this.dragElement = target;
+                    break;
+                case "end":
+                    this.dragElement = undefined;
+                    break;
+                case "over":
+                    this.dropElement = target;
+                    break;
+                case "leave":
+                    this.dropElement = undefined;
+                    break;
+                default:
+                    throw new Error(`There is no Options ${state}`);
+            }
         });
     }
 
